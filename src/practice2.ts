21d6ad78e8a404e1d6ff4d07dc3039e499cdce40
sanga.park <sagna.park@epikar.com>
2022-09-27 15:29:11 +0900

    배열

---
+++ src/practice2.ts
@@ -0,0 +1,21 @@
+// [함수]
+
+function  sum(x: number, y:number): number {
+    return x+y;
+    // return null;  [error]
+}
+sum(1, 2);
+
+
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
+const total = sumArray([1,2,3,4,5]);
+
+
+function returnNothing(): void {
+    console.log('i am just saying hollo world');
+}
+
+
+
